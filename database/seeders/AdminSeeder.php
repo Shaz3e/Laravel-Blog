de342db751720a491429b2f2cf8bcd06a1f5e976
Shahrukh A. Khan <iam@shaz3e.com>
2023-10-20 00:57:00 +0500

    Hash Password updated with seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -19,31 +20,31 @@ class AdminSeeder extends Seeder
             [
                 'name' => 'Super Admin',
                 'email' => 'superadmin@email.com',
-                'password' => 'password',
+                'password' => Hash::make('password'),
                 'role_id' => '1',
             ],
             [
                 'name' => 'Admin',
                 'email' => 'admin@email.com',
-                'password' => 'password',
+                'password' => Hash::make('password'),
                 'role_id' => '2',
             ],
             [
                 'name' => 'manager',
                 'email' => 'manager@email.com',
-                'password' => 'password',
+                'password' => Hash::make('password'),
                 'role_id' => '3',
             ],
             [
                 'name' => 'staff',
                 'email' => 'staff@email.com',
-                'password' => 'password',
+                'password' => Hash::make('password'),
                 'role_id' => '4',
             ],
             [
                 'name' => 'user',
                 'email' => 'user@email.com',
-                'password' => 'password',
+                'password' => Hash::make('password'),
                 'role_id' => '5',
             ],
         ];
