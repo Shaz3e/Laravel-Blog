7b176e9461d08e13bd3e601288a1a41ec354d3ca
Shahrukh A. Khan <iam@shaz3e.com>
2023-10-20 20:06:01 +0500

    tags migration updated

--- database/migrations/2023_10_19_135529_create_tags_table.php
+++ database/migrations/2023_10_19_135529_create_tags_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->boolean('is_active')->default(1);
             $table->timestamps();
         });
     }
